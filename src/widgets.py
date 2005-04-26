c78e7c1febf5629f9b6be1bb9f94741b7e3626ac
trygvebw <trygvebw@1994eaa7-90f5-0310-bc53-cb133adad423>
2005-04-26 13:35:47 +0000

    initial import

---
+++ src/widgets.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import sys
+import os
+import time
+import pygtk
+import gtk
+import gtk.glade
+import gobject
+
+rootdir = os.getcwd()
+sys.path.append('..')
+sys.path.append(rootdir + 'art')
+
+import libs
+import variables
+import colours
+
+#def importObjects():
+#	widgets = xml.get_widget('colorbox')
+
+def createWindow(window="dtinstaller"):
+	xml = gtk.glade.XML(rootdir + '/src/gui/gui.glade', window)
+	colorbox = xml.get_widget('colorbox')
+	widgets = xml.get_widget_prefix('')
+
+	e = gtk.Entry()
+	map = e.get_colormap()
+	colour = map.alloc_color("#FF9999") # light red
+	gtk.EventBox.modify_bg(colorbox, "GTK_STATE_NORMAL", colour)
+	
+	signal_handlers = {
+		'on_' + window + '_delete_event': libs.quitProg
+		}
+
+	xml.signal_autoconnect(signal_handlers)
