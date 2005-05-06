a898def16dc73dc148f9dbe65e73dbbd18efb8bf
trygvebw <trygvebw@1994eaa7-90f5-0310-bc53-cb133adad423>
2005-05-06 08:42:26 +0000

    Major update.

--- trunk/src/widgets.py
+++ trunk/src/widgets.py
@@ -1,3 +1,29 @@
+# Copyright (c) 2000 Trygve B. Wiig <trygvebw@gmail.com>
+# All rights reserved.
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+
+# 1. Redistributions of source code must retain the above copyright notice, this list of 
+# conditions # and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice, this list of 
+# conditions and the following disclaimer in the documentation and/or other materials provided 
+# with the distribution.
+# 3. The name of the author may not be used to endorse or promote products derived from this 
+# software without specific prior written permission.
+
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+# IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF 
+# USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
+# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 #!/usr/bin/env python
 
 import sys
@@ -28,10 +54,8 @@ class WidgetActions:
 	@license: BSDL"""
 
 	xml = gtk.glade.XML(libs.rootdir + '/src/gui/gui.glade', "dtinstaller")
-	colourbox = xml.get_widget('colorbox')
 	quitmenubt = xml.get_widget('quit1')
 	notebook = xml.get_widget('notebook')
-	colourtopbox = xml.get_widget('coloreventbox')
 	quitbt = xml.get_widget('quitbt')
 	widgets = xml.get_widget_prefix('')
 
@@ -41,12 +65,10 @@ class WidgetActions:
 		"""This function creates all the windows and widgets and configures them."""
 	
 		self.notebook.set_show_tabs(0)
-		e = gtk.Entry()
-		map = e.get_colormap()
-		colourRightBar = map.alloc_color(colours.rightbar) # blue
-		colourTopBar = map.alloc_color(colours.topbar) # dark blue
-		gtk.EventBox.modify_bg(self.colourbox, "GTK_STATE_NORMAL", colourRightBar)
-		gtk.EventBox.modify_bg(self.colourtopbox, "GTK_STATE_NORMAL", colourTopBar)
+#		e = gtk.Entry()
+#		map = e.get_colormap()
+#		colourRightBar = map.alloc_color(colours.rightbar) # blue
+#		gtk.EventBox.modify_bg(self.colourbox, "GTK_STATE_NORMAL", colourRightBar)
 		
 		## gtk event/signal checking/connecting/handling	
 	
@@ -70,6 +92,7 @@ class WidgetActions:
 			
 			"""This function creates a message window."""
 			
+			print "fake"
 			f = gtk.Entry()
 			map2 = f.get_colormap()
 			xml2 = gtk.glade.XML(libs.rootdir + '/src/gui/gui.glade', "msgwindow")
